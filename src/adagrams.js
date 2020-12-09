c73a4ae717b4e37b00ed3127cb157e33731070a9
Pauline Chane <pauline.chane@gmail.com>
2020-12-09 04:14:23 -0800

    scoring method seems to work? tests are passing...

--- src/adagrams.js
+++ src/adagrams.js
@@ -56,18 +56,24 @@ const Adagrams = {
   },
 
   scoreWord(word) {
-    // key/values pairs of scores to letters assoc. with score
-    const scoreHash = { 1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
-                        2: ['D', 'G'],
-                        3: ['B', 'C', 'M', 'P'], 
-                        4: ['F', 'H', 'V', 'W', 'Y'], 
-                        5: ['K'],
-                        8: ['J', 'X'],
-                        10: ['Q', 'Z'] }
+    // key/values pairs of letters to score score
+    const scoreHash = { A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
+                        D: 2, G: 2,
+                        B: 3, C: 3, M: 3, P: 3,
+                        F: 4, H: 4, V: 4, W: 4, Y: 4,
+                        K: 5,
+                        J: 8, X: 8,
+                        Q: 10, Z: 10}
+
     // starting score 8 pts if word is at least 7 letters long
     let score = word.length > 6 ? 8 : 0;
 
+    // iterate through each letter of word
+    for(const letter of word) {
+      score += scoreHash[letter.toUpperCase()];
+    }
 
+    return score; 
   },
 
   highestScoreFrom(words) {
