4a36dc954a39fba50c011cf15aa0c5dec3a0fdc0
Pauline Chane <pauline.chane@gmail.com>
2020-12-09 03:33:31 -0800

    drawLetters is working

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,7 +1,8 @@
 const Adagrams = {
   
   drawLetters() {
-    // hash to store frequencies of letters
+    // hash to store frequencies of letters (values) and weights (keys)
+    // probably could have just made one big array, but this felt more readable? 
     const letterFreq = {1: ['J', 'K', 'Q', 'X', 'Z'], 
                         2: ['B', 'C', 'F', 'H', 'M', 'P', 'V', 'W', 'Y'], 
                         3: ['G'], 
@@ -10,14 +11,24 @@ const Adagrams = {
                         8: ['O'],
                         9: ['A', 'I'],
                         12: ['E']};
-
+    
+    // create array with "weighted" letters
     let letArray = [];
+
+    // for each key/value pair
     for(const [key, value] of Object.entries(letterFreq)){
-      console.log(Array(Number(key)).fill(0));
+      // shovel array to letArray with key times of each letter. 
       value.forEach(letter => letArray = letArray.concat(Array(Number(key)).fill(letter)));
     }
+    
+    // delete random elements from letArray until only 10 left
+    while( letArray.length > 10) 
+    {
+      let deleteIndex = Math.floor(Math.random() * letArray.length);
+      letArray.splice(deleteIndex, 1);
+    }
 
-    console.log(letArray)
+    return letArray;
   },
 };
 
