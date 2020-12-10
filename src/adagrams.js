eb63d0de4070f6b70166406339e5ea3e2182af5e
KayKay-git <kayalew@yahoo.com>
2020-12-09 20:46:17 -0500

    Implementeed drawLetters function; pass

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,48 @@
 const Adagrams = {
+// WAVE 1
   drawLetters() {
-    // Implement this method for wave 1
+
+    const letterPool = {
+      A: 9,
+      B: 2,
+      C: 2,
+      D: 4, 
+      E: 12, 
+      F: 2,
+      G: 3, 
+      H: 2,
+      I: 9, 
+      J: 1,
+      K: 1,
+      L: 4,
+      M: 2,
+      N: 6,
+      O: 8,
+      P: 2,
+      Q: 1,
+      R: 6,
+      S: 4,
+      T: 6,
+      U: 4,
+      V: 2,
+      W: 2,
+      X: 1,
+      Y: 2,
+      Z: 1 
+    };
+  
+  let letters = Object.keys(letterPool);
+  let handOfLetters = [];
+
+// draw 10 random letters 
+while (handOfLetters.length < 10) {
+  let randomLetter = letters[Math.floor(Math.random() * letters.length)];
+    if (letterPool[randomLetter] > 0) {
+        letterPool[randomLetter] -= 1;
+        handOfLetters.push(randomLetter);
+      }
+    }
+    return handOfLetters; 
   },
 };
 
