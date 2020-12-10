b9a23c0c11ae1627ab94c555434bf20d4068a0a5
christinaminh <christina.minh@gmail.com>
2020-12-10 14:39:57 -0800

    Changed Adagrams from object into class. Changed export line:

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,32 +1,45 @@
-const Adagrams = {
-  letterDistribution: {
-    A: 9,	
-    B: 2,	
-    C: 2,
-    D: 4,	
-    E: 12,	
-    F: 2,
-    G: 3,	
-    H: 2,	
-    I: 9,	
-    J: 1,	
-    K: 1,	
-    L: 4,	
-    M: 2,	
-    N: 6,
-    O: 8,
-    P: 2,
-    Q: 1,
-    R: 6,
-    S: 4,
-    T: 6,
-    U: 4,
-    V: 2,
-    W: 2,
-    X: 1,
-    Y: 2,
-    Z: 1,
-  },
+class Adagrams {
+  constructor() {
+    this.letterDistribution = {
+      A: 9,	
+      B: 2,	
+      C: 2,
+      D: 4,	
+      E: 12,	
+      F: 2,
+      G: 3,	
+      H: 2,	
+      I: 9,	
+      J: 1,	
+      K: 1,	
+      L: 4,	
+      M: 2,	
+      N: 6,
+      O: 8,
+      P: 2,
+      Q: 1,
+      R: 6,
+      S: 4,
+      T: 6,
+      U: 4,
+      V: 2,
+      W: 2,
+      X: 1,
+      Y: 2,
+      Z: 1,
+    };
+
+    this.scoreDistribution = {
+      1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+      2: ['D', 'G'],
+      3: ['B', 'C', 'M', 'P'],
+      4: ['F', 'H', 'V', 'W', 'Y'],
+      5: ['K'],
+      8: ['J', 'X'],
+      10: ['Q', 'Z'],
+    };
+  }
+
   drawLetters() {
     let letterPool = [];
     for(const [letter, count] of Object.entries(this.letterDistribution)) {
@@ -39,7 +52,8 @@ const Adagrams = {
       hand.push(letterPool[rand_index]);
     }
     return hand;
-  },
+  }
+
   usesAvailableLetters(input, lettersInHand) {
     const lettersToUse = {};
     for(const letter of lettersInHand) {
@@ -59,16 +73,8 @@ const Adagrams = {
     }
 
     return true;
-  },
-  scoreDistribution: {
-    1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
-    2: ['D', 'G'],
-    3: ['B', 'C', 'M', 'P'],
-    4: ['F', 'H', 'V', 'W', 'Y'],
-    5: ['K'],
-    8: ['J', 'X'],
-    10: ['Q', 'Z'],
-  },
+  }
+
   scoreWord(word) {
     const letterPointValues = {};
 
@@ -87,7 +93,8 @@ const Adagrams = {
     }
 
     return score;
-  },
+  }
+
   highestScoreFrom(words) {
     const winner = { 
       word: '',
@@ -98,18 +105,18 @@ const Adagrams = {
       const score = this.scoreWord(word)
 
       if (score > winner.score) {
-        winner.score = score
-        winner.word = word
+        winner.score = score;
+        winner.word = word;
       } else if (score === winner.score && 
         winner.word.length != 10 && 
         ( word.length === 10 || word.length < winner.word.length)) {
-        winner.score = score
-        winner.word = word
+        winner.score = score;
+        winner.word = word;
       }
     });
     return winner;
   }
-};
+}
 
 // Do not remove this line or your tests will break!
-export default Adagrams;
+export default new Adagrams();
