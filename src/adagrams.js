0b20ff80aede1789d9f2ac79f430e63dcf566ff2
christinaminh <christina.minh@gmail.com>
2020-12-10 10:22:42 -0800

    Added drawLetters function

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,8 +1,56 @@
+// import { map } from "core-js/fn/array";
+
 const Adagrams = {
-  drawLetters() {
-    // Implement this method for wave 1
+  letterDistribution: {
+    A: 9,	
+    B: 2,	
+    C: 2,
+    D: 4,	
+    E: 12,	
+    F: 2,
+    G: 3,	
+    H: 2,	
+    I: 9,	
+    J: 1,	
+    K: 1,	
+    L: 4,	
+    M: 2,	
+    N: 6,
+    O: 8,
+    P: 2,
+    Q: 1,
+    R: 6,
+    S: 4,
+    T: 6,
+    U: 4,
+    V: 2,
+    W: 2,
+    X: 1,
+    Y: 2,
+    Z: 1,
   },
+  drawLetters() {
+    const letterPool = ( () => {
+      let pool = [];
+      for(const [letter, count] of Object.entries(this.letterDistribution)) {
+        pool = pool.concat(Array.from(letter.repeat(count)));
+      }
+      return pool;
+    })();
+
+    const hand = [];    
+
+    for(let i = 0; i < 10; i += 1 ) {
+      const rand_index = Math.floor(Math.random() * letterPool.length);
+      hand.push(letterPool[rand_index]);
+    }
+
+    return hand
+  },
+  
 };
 
 // Do not remove this line or your tests will break!
-export default Adagrams;
+// export default Adagrams;
+
+console.log(Adagrams.drawLetters());
