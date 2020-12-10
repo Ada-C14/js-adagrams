6da7bf0baf80cf496616634a5e9445a129e92c1f
India Kato <india.kato@gmail.com>
2020-12-10 14:24:33 -0800

    drawLetters function with tests passing

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,7 +1,44 @@
+const LETTER_HASH = {
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1
+}
 const Adagrams = {
   drawLetters() {
-    // Implement this method for wave 1
-  },
+    const letters = [];
+
+    for (const [key, value] of Object.entries(LETTER_HASH)) {
+      for (let i = 0; i < value + 1; i += 1) {
+        letters.push(key);
+      }
+    }
+
+    letters.sort(function(a, b) {return 0.5 - Math.random()});
+    return letters.slice(0, 10);
+  }
 };
 
 // Do not remove this line or your tests will break!
