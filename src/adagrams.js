6063fbc19983a2c749aa82f1ffd11b84b40aab0c
Schanen Ryan <schanen.r@gmail.com>
2020-12-10 23:35:03 -0800

    scoreWord all tests passing

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,11 +1,11 @@
 
 const lettersCount = {"A": 9, "B": 2, "C": 2, "D": 4, "E": 12, "F": 2, "G": 3, "H": 2, "I": 9, "J": 1, "K": 1, "L": 4, "M": 2, "N": 6, "O": 8, "P": 2, "Q": 1, "R": 6, "S": 4, "T": 6, "U": 4, "V": 2, "W": 2, "X": 1, "Y": 2, "Z": 1};
+const lettersScore = {"A": 1, "E": 1, "I": 1, "O": 1, "U": 1, "L": 1, "N": 1, "R": 1, "S": 1, "T": 1, "D": 2, "G": 2, "B": 3, "C": 3, "M": 3, "P": 3, "F": 4, "H": 4, "V": 4, "W": 4, "Y": 4,"K": 5, "J": 8, "X": 8,"Q": 10, "Z": 10};
 
 
 const Adagrams = {
 
   drawLetters() {
-    // Implement this method for wave 1
     let total_letters = [];
 
     for (const letter in lettersCount) {
@@ -23,7 +23,7 @@ const Adagrams = {
       const i = Math.floor(Math.random() * max--);
       [array[max], array[i]] = [array[i], array[max]];
     };
-    // I'm confused about why an index is needed for .slice and how to use it
+
     return array.slice(0, hand);
   },
 
@@ -49,7 +49,22 @@ const Adagrams = {
     };
     console.log(tally);
     return true;
+  },
+
+  scoreWord(word) {
+    let sum = 0
+    word = word.toUpperCase().split('')
+    
+    if (word.length > 6) {sum += 8};
+
+    for(const letter of word) {
+      sum += lettersScore[letter];
+    };
+
+    return sum
   }
+    
+
 };
 
 // Adagrams.drawLetters();
