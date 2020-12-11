00e557c12b1b8e8bb3c4752d98ca741b4be28fe9
Lina Do <l.do5147@gmail.com>
2020-12-10 23:57:54 -0800

    added an object listing scores for each letter

--- src/adagrams.js
+++ src/adagrams.js
@@ -27,6 +27,35 @@ const letterQuantities = {
   'Z': 1
 };
 
+const letterScore = {
+  'A': 1,
+  'B': 3,
+  'C': 3,
+  'D': 2,
+  'E': 1,
+  'F': 4,
+  'G': 2,
+  'H': 4,
+  'I': 1,
+  'J': 8,
+  'K': 5,
+  'L': 1,
+  'M': 3,
+  'N': 1,
+  'O': 1,
+  'P': 3,
+  'Q': 10,
+  'R': 1,
+  'S': 1,
+  'T': 1,
+  'U': 1,
+  'V': 4,
+  'W': 4,
+  'X': 8,
+  'Y': 4,
+  'Z': 10
+};
+
 
 const Adagrams = {
   drawLetters() {
@@ -79,7 +108,11 @@ const Adagrams = {
 
   },
 
+  scoreWord(word) {
+    letterScore = {
 
+    }
+  }
 
 
 };
