fa7cf91c65bdc50827497fa4362b5bea77a55d68
Mackenzie <mvlofthus@gmail.com>
2020-12-11 00:11:58 -0800

    un-commented export line and tests run again, changed alphabet to all caps to pass second test

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,10 +1,10 @@
 const letterPool = {
-  alphabet: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+  alphabet: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
   quantities: [9,2,2,4,12,2,3,2,9,1,1,4,2,6,8,2,1,6,4,6,4,2,2,1,2,1],
   build () {
     let letterPool = [];
 
-    const buildLetters = () => {
+    const fillLetterPool = () => {
       for (const index in this.alphabet) {
         for (let i=0; i < this.quantities[index]; i++) {
           letterPool.push(this.alphabet[index]);
@@ -12,7 +12,7 @@ const letterPool = {
     return letterPool;
     };
 
-    buildLetters();
+    fillLetterPool();
 
     return letterPool;
   },
@@ -21,28 +21,24 @@ const letterPool = {
 const Adagrams = {
   drawLetters() {
     // Implement this method for wave 1
-    // #p letters_pool
-    // drawn_letters = letters_pool.sample(10)
-    // return drawn_letters
   
     let letters = letterPool.build();
-    console.log(letters);
+    // console.log(letters);
     
     let hand = [];
     
     for (let i=0; i < 10; i++) {
       hand.push(letters[Math.floor(Math.random() * letters.length)]); 
     }
-    console.log(hand);
+    // console.log(hand);
     return hand;
-    },
-  
+    }
 };
 
-Adagrams.drawLetters();
-
+// console.log(Adagrams.drawLetters());
+// console.log(Adagrams.drawLetters().length);
 
 
 
 // Do not remove this line or your tests will break!
-// export default Adagrams;
+export default Adagrams;
