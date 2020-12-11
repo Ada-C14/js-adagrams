aa8d0ad950ed9439b7149a5bbf2a13b9282c7cfe
Ida <ida.goitom@yahoo.com>
2020-12-11 15:44:38 -0800

    updated draw letters function to return 10 letters

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,7 +1,53 @@
+const a = new Array('A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A')
+const b = new Array('B', 'B')
+const c = new Array('C', 'C')
+const d = new Array('D', 'D', 'D', 'D')
+const e = new Array('E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E')
+const f = new Array('F', 'F')
+const g = new Array('G', 'G', 'G')
+const h = new Array('H', 'H')
+const i = new Array('I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I')
+const j = new Array('J')
+const k = new Array('K')
+const l = new Array('L', 'L', 'L', 'L')
+const m = new Array('M', 'M')
+const n = new Array('N', 'N', 'N', 'N', 'N', 'N')
+const o = new Array('O', 'O', 'O', 'O', 'O', 'O', 'O', 'O')
+const p = new Array('P', 'P')
+const q = new Array('Q')
+const r = new Array('R', 'R', 'R', 'R', 'R', 'R')
+const s = new Array('S', 'S', 'S', 'S')
+const t = new Array('T', 'T', 'T', 'T', 'T', 'T')
+const u = new Array('U', 'U', 'U', 'U')
+const v = new Array('V', 'V')
+const w = new Array('W', 'W')
+const x = new Array('X')
+const y = new Array('Y', 'Y')
+const z = new Array('Z')
+
+const alphabetArray  = function () { 
+return  [...a, ...b, ...c, ...d, ...e, ...f, ...g, ...h, ...i, ...j, ...k, ...l, ...m, ...n,
+  ...o, ...p, ...q, ...r, ...s, ...t, ...u, ...v, ...w, ...x, ...y, ...z] //spread operator
+}
+
 const Adagrams = {
   drawLetters() {
-    // Implement this method for wave 1
+    // const ten_letters = _.shuffle(alphabet)[10];
+    // return ten_letters
+    const alphabet = alphabetArray();
+    const ten_letters = []
+    for(let i= 0; i < 10; i += 1) {
+      let randomIndex = Math.floor((Math.random() * alphabet.length)) //pick a random number
+      let temp = alphabet[randomIndex]
+      let lastIndex = alphabet.length - 1
+
+      alphabet[randomIndex] = alphabet[lastIndex]
+      alphabet.pop()
+      ten_letters.push(temp)
+    }
+    return ten_letters
   },
+
 };
 
 // Do not remove this line or your tests will break!
