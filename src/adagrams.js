a35f60dd953bfdc5fab4494f11eed508612a1db6
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-12-11 23:05:50 -0800

    implement scoreWord

--- src/adagrams.js
+++ src/adagrams.js
@@ -29,6 +29,35 @@ const ALL_LETTERS = {
   Z: 1
 }
 
+const LETTER_SCORES = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10
+}
+
 class Adagrams {
   drawLetters() {
     const allLetters = [];
@@ -68,6 +97,12 @@ class Adagrams {
     }
 
     return true;
+  }
+
+  scoreWord(word) {
+    const score = word.toUpperCase().split('').reduce((score, letter) => score + LETTER_SCORES[letter], 0);
+
+    return word.length >= 7 ? score + 8 : score;
   }
 }
 
