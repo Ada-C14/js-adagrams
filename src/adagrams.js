8d01aa0b75133b24bba847c564183c59e86d9417
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-12-11 21:00:01 -0800

    usesAvailbleLetters wave complete. Created remove items function

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,41 +1,68 @@
-import has from 'lodash/has';
 import _ from 'lodash';
+// var _ = require('lodash');
 
     //letters/frequencies object
     const LETTERS = {A : 9, B : 2, C : 2, D : 4, E : 12, F : 2, G : 3, H : 2, I : 9, J : 1, K : 1, L : 4, M : 2, N : 6, O : 8, P : 2, Q : 1, R : 6, S : 4, T : 6, U : 4, V : 2, W : 2, X : 1, Y : 2, Z : 1 };
 
+      //method to remove specific value from array
+  function removeItemsOnce(array, value) {
+    let index = array.indexOf(value);
+    array.splice(index, 1);
+    return array;
+  };
+
+
 const Adagrams = {
   drawLetters() {
-
-    //Letters are now being populated for game play! Start with an empty array.
+    
+    //Letters will now be populated for game play! Start with an empty array. Pulling from const LETTERS hash.
     let letters = [];
 
-    //now populating array with the correct number of letters
+    //populate array with the correct number of letters (frequencies)
     for (let [key] in LETTERS) {
       for(let i = 1; i <= LETTERS[key]; i++) {
         letters.push(key);
+        
       }
     };
-    //need to create a 'sample' method to return a sampling of tiles
+    //create a 'sample' method to return a sampling of tiles
     const randomTenTiles = _.sampleSize; //_.sampleSize is lodash npm
-    const drawn = randomTenTiles(letters, 10);
+    let drawn = randomTenTiles(letters, 10);
     return drawn;
   },
 
+  //input = word played
   usesAvailableLetters(input, lettersInHand) {
-    const drawn_tiles = input;
-    let used_tiles = Array.from(drawn_tiles);
-
-
+    let playedTiles = input.toUpperCase().split('');
+    let dupLettersInHand = Array.from(lettersInHand);
+    // console.log(playedTiles);
+    if (playedTiles.length <= lettersInHand.length) {
+      for (let letter of playedTiles) {
+        if (dupLettersInHand.includes(letter)) {
+          removeItemsOnce(dupLettersInHand, letter); //calling function to delete tile from copied array. 
+        }
+        else {
+          return false;
+        };
+      };
+      return true;
+      
+        return false;
+      };
+      {
+      return false;
+      };
+    }
   }
 
-};
+
+
 
 // Do not remove this line or your tests will break!
 export default Adagrams;
 
 
-
+// Adagrams.usesAvailableLetters('test', Adagrams.drawLetters());
 
 
 
@@ -44,3 +71,4 @@ export default Adagrams;
 //2. lodash _.sampleSize: https://lodash.com/docs/4.17.15 
 //3. Chris was used as a reference for learning how to get lodash to work w/jest :)
 //4. creating duplicate array (number 10-Array.from): https://www.freecodecamp.org/news/how-to-clone-an-array-in-javascript-1d3183468f6a/ 
+//Removing value from array: https://stackoverflow.com/questions/5767325/how-can-i-remove-a-specific-item-from-an-array
