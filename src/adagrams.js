73b9b436d45d48784d8226e14aecda4a0b280e41
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-12-11 21:26:33 -0800

    scoreWord function complete and all tests passing

--- src/adagrams.js
+++ src/adagrams.js
@@ -52,6 +52,60 @@ const Adagrams = {
       {
       return false;
       };
+    },
+  scoreWord(word) {
+    let word_array = word.toUpperCase().split('');
+    let score = 0;
+
+    if (word_array.length > 6) {
+      score += 8;
+    };
+    for (let letter of word_array) {
+      switch (letter) {
+        case "A":
+        case "E":
+        case "I":
+        case "O":
+        case "U":
+        case "L":
+        case "N":
+        case "R":
+        case "S":
+        case "T":
+          score += 1;
+          break;
+        case "D":
+        case "G":
+          score += 2;
+          break;
+        case "B":
+        case "C":
+        case "M":
+        case "P":
+          score += 3;
+          break;
+        case "F":
+        case "H":
+        case "V":
+        case "W":
+        case "Y":
+          score += 4;
+          break;
+        case "K":
+          score += 5;
+          break;
+        case "J":
+        case "X":
+          score += 8;
+          break;
+        case "Q":
+        case "Z":
+          score += 10;
+          break;
+    }
+};
+return score;
+
     }
   }
 
