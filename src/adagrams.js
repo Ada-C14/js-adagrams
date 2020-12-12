bbebd01271145dacdbdf5b8413598536d5cb8e1a
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-12-11 22:05:41 -0800

    implement drawLetters

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,46 @@
+import shuffle from 'shuffle-array';
+
+const ALL_LETTERS = {
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1
+}
+
 const Adagrams = {
   drawLetters() {
-    // Implement this method for wave 1
+    const allLetters = [];
+
+    for (const [letter, count] of Object.entries(ALL_LETTERS)) {
+      for (let i = 0; i < count; i++) {
+        allLetters.push(letter); 
+      }
+    }    
+    
+    // Return a random collection with 10 elements
+    return shuffle.pick(allLetters, { picks: 10 }); 
   },
 };
 
