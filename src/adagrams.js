f0bddb7f70081d3f2702d602b8483063fa33733b
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2020-12-12 12:24:49 -0500

    wave 2

--- src/adagrams.js
+++ src/adagrams.js
@@ -2,32 +2,32 @@ const Adagrams = {
   drawLetters() {
     // Implement this method for wave 1
     const letterQuan = {
-        A: 9,
-        B: 2,
-        C: 2,
-        D: 4,
-        E: 12,
-        F: 2,
-        G: 3,
-        H: 2,
-        I: 9,
-        J: 1,
-        K: 1,
-        L: 4,
-        M: 2,
-        N: 6,
-        O: 8,
-        P: 2,
-        Q: 1,
-        R: 6,
-        S: 4,
-        T: 6,
-        U: 4,
-        V: 2,
-        W: 2,
-        X: 1,
-        Y: 2,
-        Z: 1
+      A: 9,
+      B: 2,
+      C: 2,
+      D: 4,
+      E: 12,
+      F: 2,
+      G: 3,
+      H: 2,
+      I: 9,
+      J: 1,
+      K: 1,
+      L: 4,
+      M: 2,
+      N: 6,
+      O: 8,
+      P: 2,
+      Q: 1,
+      R: 6,
+      S: 4,
+      T: 6,
+      U: 4,
+      V: 2,
+      W: 2,
+      X: 1,
+      Y: 2,
+      Z: 1
     };
     // create a pool of letters to draw from
     const letterPool = []
@@ -47,27 +47,77 @@ const Adagrams = {
     // draw 10 letters
     const lettersInHand = letterPool.slice(0, 10);
 
-    return hand
+    return lettersInHand
   },
 
   usesAvailableLetters(input, lettersInHand) {
-    const hashMap = {}
+    const handHashMap = {}
 
     for (let i = 0; i < lettersInHand.length; i++) {
-      hashMap[lettersInHand[i]] = 0;
-    }
+      handHashMap[lettersInHand[i]] = 0 };
 
-    for (let letter of lettersInHand) {
-      hashMap[letter] += 1
-    }
-    console.log(hashMap)
-    console.log(input)
+    for (let i = 0; i < lettersInHand.length; i++) {
+      handHashMap[lettersInHand[i]] += 1 };
+
+    for (const letter of input) {
+      if (handHashMap[letter]) {
+        handHashMap[letter] -= 1
+        // use debugger to learn why it never hits line 66
+        if (handHashMap[letter] < 0) { 
+          console.log(`Not enough of letter ${letter} in hand!`)
+          return false } }
+      else {
+        console.log(`${letter} was never in hand!`)
+        return false }
+      };
+    return true;
   }
 
+  // scoreWord
+
+  scoreWord(word) {
+    const letterScore = {
+      A: 1,
+      B: 3,
+      C: 3,
+      D: 2,
+      E: 1,
+      F: 4,
+      G: 2,
+      H: 4,
+      I: 1,
+      J: 8,
+      K: 5,
+      L: 1,
+      M: 3,
+      N: 1,
+      O: 1,
+      P: 3,
+      Q: 10,
+      R: 1,
+      S: 1,
+      T: 1,
+      U: 1,
+      V: 4,
+      W: 4,
+      X: 8,
+      Y: 4,
+      Z: 10
+    };
 
 
 
-};
+
+
+  }
+
+  
+
+
+}
+
+// console.log(Adagrams.drawLetters())
+
 
 // Do not remove this line or your tests will break!
 export default Adagrams;
