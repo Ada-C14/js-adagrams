7130aa68f622e15f0e54f70e26ca5ae0d3c37542
Ida <ida.goitom@yahoo.com>
2020-12-12 12:40:14 -0800

    added score chart object

--- src/adagrams.js
+++ src/adagrams.js
@@ -31,6 +31,16 @@ return  [...a, ...b, ...c, ...d, ...e, ...f, ...g, ...h, ...i, ...j, ...k, ...l,
   // ... = spread operator,  makes it a one dimensional array
 }
 
+const scoreChart = {
+  A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
+  D: 2, G: 2,
+  B: 3, C: 3, M: 4, P: 3,
+  F: 4, H: 4, V: 4, W: 4, Y: 4,
+  K: 5,
+  J: 8, X: 8,
+  Q: 10, Z: 10,
+  }
+
 const Adagrams = {
   drawLetters() {
   
@@ -62,7 +72,6 @@ const Adagrams = {
         letterCount[currentLetter] = count
       } 
     }
-        console.log(letterCount)
     return true        
 
   },
@@ -78,6 +87,21 @@ const Adagrams = {
       }
      }
      return letterFrequency
+  },
+  
+  scoreWord(word) {
+    for (letter in word) {
+   
+      const scoreChart = {
+        1: [A, E, I, O, U, L, N, R, S, T],
+        2: [D, G],
+        3: [B, C, M, P],
+        4: [F, H, V, W, Y],
+        5: [K],
+        8: [J, X],
+        10: [Q, Z]
+        }
+    }
   }
 
 };
