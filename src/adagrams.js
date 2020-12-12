6b22a9cfdd746a89c5a48e1b7b6f15d2967bca77
Ida <ida.goitom@yahoo.com>
2020-12-12 14:15:09 -0800

    added scoreword method with all tests passing

--- src/adagrams.js
+++ src/adagrams.js
@@ -34,7 +34,7 @@ return  [...a, ...b, ...c, ...d, ...e, ...f, ...g, ...h, ...i, ...j, ...k, ...l,
 const scoreChart = {
   A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
   D: 2, G: 2,
-  B: 3, C: 3, M: 4, P: 3,
+  B: 3, C: 3, M: 3, P: 3,
   F: 4, H: 4, V: 4, W: 4, Y: 4,
   K: 5,
   J: 8, X: 8,
@@ -90,21 +90,19 @@ const Adagrams = {
   },
   
   scoreWord(word) {
-    for (letter in word) {
-   
-      const scoreChart = {
-        1: [A, E, I, O, U, L, N, R, S, T],
-        2: [D, G],
-        3: [B, C, M, P],
-        4: [F, H, V, W, Y],
-        5: [K],
-        8: [J, X],
-        10: [Q, Z]
-        }
+    let total = 0;
+    for (const letter of word) {
+      
+        total += scoreChart[letter.toUpperCase()];
     }
+    if (word.length > 6) {
+        total += 8;
+    }
+    return total;
   }
-
 };
+
+// console.log(scoreWord('ida'))
 
 // const usesAvailableLetters = function(input, lettersInHand) {
 //   const letterCount = countLetters(lettersInHand)
