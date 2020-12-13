0e1b27b97bbb5200ab6cc5efa9948cf11723cb62
OlgaSe <olga.tka4eva@gmail.com>
2020-12-12 17:34:58 -0800

    wave 3 completed, tests are passing

--- src/adagrams.js
+++ src/adagrams.js
@@ -57,7 +57,6 @@ const Adagrams = {
         }
       }
       const input_array = input.split('');
-      console.log(input_array);
 
       for (let letter of input_array) {
         if (object[letter] > 0) {
@@ -67,7 +66,54 @@ const Adagrams = {
         }
       }
       return true;
-   } 
+  },
+  
+  scoreWord(word) {
+    const scores = {
+      "A" : 1,
+      "B" : 3,
+      "C" : 3,
+      "D" : 2,
+      "E" : 1,
+      "F" : 4,
+      "G" : 2,
+      "H" : 4,
+      "I" : 1,
+      "J" : 1,
+      "K" : 8,
+      "L" : 1,
+      "M" : 3,
+      "N" : 1,
+      "O" : 1,
+      "P" : 3,
+      "Q" : 10,
+      "R" : 1,
+      "S" : 1,
+      "T" : 1,
+      "U" : 1,
+      "V" : 4,
+      "W" : 4,
+      "X" : 8,
+      "Y" : 4,
+      "Z" : 10
+    }
+   
+    let word_array = word.toUpperCase().split('');
+    let sum = 0;
+
+    if (word_array.length >= 7) {
+      sum += 8;
+    }
+
+    for(let letter of word_array) {
+      if (scores[letter]) {
+        sum += scores[letter];
+      }
+    }
+    return sum;
+  }
+
+
 };
 
 // console.log(Adagrams.drawLetters())
