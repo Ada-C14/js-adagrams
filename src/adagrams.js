374edfbf8b19ab24282ecb1c27d4329ffce76fd8
Li Dai <li.lea.dai@gmail.com>
2020-12-12 20:22:13 -0800

    highestScoreFrom done

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,10 +1,10 @@
 const Adagrams = {
-
   drawLetters() {
     let letterPool = buildPool();
     const shuffled = letterPool.sort(() => 0.5 - Math.random());
     return shuffled.slice(0, 10);
   },
+
   usesAvailableLetters(input, lettersInHand) {
     let counter = counterBuilder(lettersInHand);
     for (let char of input){
@@ -16,9 +16,69 @@ const Adagrams = {
     };
     return true;
   },
-};
 
+  scoreWord(word) {
+    const scores = {
+      A: 1,
+      B: 3,
+      C: 3,
+      D: 2,
+      E: 1,
+      F: 4,
+      G: 2,
+      H: 4,
+      I: 1,
+      J: 8,
+      K: 5,
+      L: 1,
+      M: 3,
+      N: 1,
+      O: 1,
+      P: 3,
+      Q: 10,
+      R: 1,
+      S: 1,
+      T: 1,
+      U: 1,
+      V: 4,
+      W: 4,
+      X: 8,
+      Y: 4,
+      Z: 10
+  };
+  word = word.toUpperCase();
+  let score = 0;
+  for (const char of word) {
+    score += scores[char];
+  }
 
+  if (word.length >= 7) {
+    score += 8;
+  }
+
+  return score;
+  },
+
+  highestScoreFrom(words) {
+    const winning_word = {
+      word: '',
+      score: 0
+    };
+    let hightestScore = 0;
+
+    for (const word of words) {
+      let score = this.scoreWord(word)
+      if (score > hightestScore) {
+        hightestScore = score;
+        winning_word['word'] = word;
+        winning_word['score'] = score;
+    } else if (score === hightestScore && winning_word.word.length !== 10) {
+      if (word.length === 10 || word.length < winning_word.word.length) {
+        winning_word['word'] = word;
+        winning_word['score'] = score;
+      }}};
+    return winning_word
+  }};
 
 const buildPool = function () {
   let letterPool = [];
@@ -59,7 +119,7 @@ const buildPool = function () {
 
 
 const counterBuilder = function(str) {
-  let counter = {};
+  const counter = {};
   for (let char of str){
     if (counter[char]) {
     counter[char] += 1
