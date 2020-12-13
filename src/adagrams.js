df2131ceb8b51ace6397441445e9568abd3fd9f8
Anna Kim <anna.kim93@gmail.com>
2020-12-12 20:51:12 -0500

    convert adagrams object to class

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,21 +1,25 @@
-const Adagrams = {
-  letterDist: {
-    A: 9, B: 2, C: 2,
-    D: 4, E: 12, F: 2,
-    G: 3, H: 2, I: 9,
-    J: 1, K: 1, L: 4,
-    M: 2, N: 6, O: 8,
-    P: 2, Q: 1, R: 6,
-    S: 4, T: 6, U: 4,
-    V: 2, W: 2, X: 1,
-    Y: 2, Z: 1
-  },
-  
-  drawLetters() {
+class Adagrams {
+  static letterDist() {
+    return {
+      A: 9, B: 2, C: 2,
+      D: 4, E: 12, F: 2,
+      G: 3, H: 2, I: 9,
+      J: 1, K: 1, L: 4,
+      M: 2, N: 6, O: 8,
+      P: 2, Q: 1, R: 6,
+      S: 4, T: 6, U: 4,
+      V: 2, W: 2, X: 1,
+      Y: 2, Z: 1
+    }
+  }
+
+  static drawLetters() {
     const letterPool = [];
-    for (const letter in this.letterDist) {
+    const letterDist = Adagrams.letterDist();
+    
+    for (const letter in letterDist) {
       let i = 0;
-      while (i < this.letterDist[letter]) {
+      while (i < letterDist[letter]) {
         letterPool.push(letter);
         i++;
       }
@@ -29,9 +33,9 @@ const Adagrams = {
     }
 
     return hand;
-  },
+  }
 
-  usesAvailableLetters(input, lettersInHand) {
+  static usesAvailableLetters(input, lettersInHand) {
     const hand = {};
     for (const letter of lettersInHand) {
       if (hand[letter.toUpperCase()]) {
@@ -50,23 +54,26 @@ const Adagrams = {
     }
 
     return true;
-  },
+  }
 
-  letterScores: {
-    A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
-    D: 2, G: 2,
-    B: 3, C: 3, M: 3, P: 3,
-    F: 4, H: 4, V: 4, W: 4, Y: 4,
-    K: 5,
-    J: 8, X: 8,
-    Q: 10, Z:10
-  },
+  static letterScores() {
+    return {
+      A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
+      D: 2, G: 2,
+      B: 3, C: 3, M: 3, P: 3,
+      F: 4, H: 4, V: 4, W: 4, Y: 4,
+      K: 5,
+      J: 8, X: 8,
+      Q: 10, Z:10
+    }
+  }
 
-  scoreWord(word) {
+  static scoreWord(word) {
     let score = 0;
+    const letterScores = Adagrams.letterScores();
     
     for (const letter of word.toUpperCase()) {
-      score += this.letterScores[letter];
+      score += letterScores[letter];
     }
 
     if (word.length > 6 && word.length < 11) {
@@ -74,15 +81,15 @@ const Adagrams = {
     }
 
     return score;
-  },
+  }
 
-  highestScoreFrom(words) {
+  static highestScoreFrom(words) {
     const scores = {};
     let maxScore = 0;
     let shortestWord;
 
     for (const word of words) {
-      scores[word] = this.scoreWord(word);
+      scores[word] = Adagrams.scoreWord(word);
       if (scores[word] > maxScore) {
         maxScore = scores[word];
       }
@@ -102,9 +109,7 @@ const Adagrams = {
 
     return { word: shortestWord, score: maxScore };
   }
-};
-
-// console.log(Adagrams.drawLetters())
+}
 
 // Do not remove this line or your tests will break!
 export default Adagrams;
