45c311e369a7bd67e49f9e42e1e19aaa061e5448
Yingjing Lin <yjlin789@gmail.com>
2020-12-13 00:21:41 -0800

    fixed tie bug

--- src/adagrams.js
+++ src/adagrams.js
@@ -66,7 +66,8 @@ const Adagrams = {
   scoreWord(word) {
     const scoreChart = {A: 1, B: 3, C: 3, D: 2, E: 1, F: 4, G: 2, H: 4, I: 1, J: 8, K: 5, L: 1, M: 3, N: 1,
                       O: 1, P: 3, Q: 10, R: 1, S: 1, T: 1, U: 1, V: 4, W: 4, X: 8, Y: 4, Z: 10};
-    let score = 0
+    let score = 0;
+    word = word.toUpperCase;
     for (const char of word) {
       score += scoreChart[char];
     };
@@ -79,7 +80,6 @@ const Adagrams = {
     for (const word of words) {
       wordWithScore[word] = this.scoreWord(word);
     };
-
     let max = 0;
     let maxWord;
     for (const word in wordWithScore) {
@@ -90,12 +90,16 @@ const Adagrams = {
         if (maxWord.length < 10 && word.length === 10) {
           maxWord = word;
         };
-        if (word.length < maxWord.length) {
+        if (word.length < maxWord.length && maxWord.length < 10) {
           maxWord = word
         };
       };
     };
-    return {maxWord: max};
+    const winner = {
+      score: max,
+      word: maxWord
+    }
+    return winner;
   },
 };
 
