14b37bf39ca9b26bf63ffcceb5eac2216aef4fc2
Taylor Tompkins <taylormililani@gmail.com>
2020-12-12 20:00:15 -0800

    tests passing for uses available letters

--- src/adagrams.js
+++ src/adagrams.js
@@ -46,78 +46,78 @@ const Adagrams = {
     return word.toUpperCase().split('').every(( index => letter => index = hand.indexOf(letter, index) + 1)(0));
   },
   
-  scoreWord(word) {
+  // scoreWord(word) {
     const onePoint = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'];
     const threePoints = ['B', 'C', 'M', 'P'];
     const fourPoints = ['F', 'H', 'V', 'W', 'Y'];
 
-    word = word.toUpperCase().split('');
-    let score = 0;
-    for (const letter of word) {
-      if (onePoint.includes(letter)) {
-        score += 1;
-      } else if (letter === 'D' || letter == 'G') {
-        score += 2;
-      } else if (threePoints.includes(letter)) {
-        score += 3;
-      } else if (fourPoints.includes(letter)) {
-        score += 4;
-      } else if (letter === 'K') {
-        score += 5;
-      } else if (letter === 'J' || letter === 'X') {
-        score += 8;
-      } else if (letter === 'Q' || letter === 'Z') {
-        score += 10;
-      }
-    }
+  //   word = word.toUpperCase().split('');
+  //   let score = 0;
+  //   for (const letter of word) {
+  //     if (onePoint.includes(letter)) {
+  //       score += 1;
+  //     } else if (letter === 'D' || letter == 'G') {
+  //       score += 2;
+  //     } else if (threePoints.includes(letter)) {
+  //       score += 3;
+  //     } else if (fourPoints.includes(letter)) {
+  //       score += 4;
+  //     } else if (letter === 'K') {
+  //       score += 5;
+  //     } else if (letter === 'J' || letter === 'X') {
+  //       score += 8;
+  //     } else if (letter === 'Q' || letter === 'Z') {
+  //       score += 10;
+  //     }
+  //   }
 
-    if (word.length > 6) {
-      score += 8;
-    }
+  //   if (word.length > 6) {
+  //     score += 8;
+  //   }
 
-    return score;
-  },
+  //   return score;
+  // },
 
-  highestScoreFrom(words) {
-    let scores = []
-    for (const word of words) {
-      scores.push(Adagrams.scoreWord(word))
-    }
+  // highestScoreFrom(words) {
+  //   let scores = []
+  //   for (const word of words) {
+  //     scores.push(Adagrams.scoreWord(word))
+  //   }
 
-    const bestScore = Math.max(...scores);
+  //   const bestScore = Math.max(...scores);
 
-    const indices = scores.flatMap((score, index) => score === bestScore ? index : []);
-    let highestScore;
-    let winningIndex;
-    let tiedWords = []
-    if (indices.length === 1) {
-      winningIndex = indices[0]
-      highestScore = {score: scores[winningIndex], word: words[winningIndex]}
-      return highestScore
-    } else {
-      for (const index of indices) {
-        tiedWords.push(words[index])
-      }
-      let shortest = tiedWords[0];
-      let shortestIndex = 0;
-      for (const word of tiedWords) {
-        if (word.length === 10) {
-          winningIndex = tiedWords.indexOf(word)
-          highestScore = {score: scores[indices[winningIndex]], word: tiedWords[winningIndex]}
-          return highestScore
-        } else {
-          if (word.length < shortest.length) {
-            shortest = word
-            shortestIndex = tiedWords.indexOf(shortest)
-          }
-        }
-      }
-      winningIndex = shortestIndex
+  //   const indices = scores.flatMap((score, index) => score === bestScore ? index : []);
+  //   let highestScore;
+  //   let winningIndex;
+  //   let tiedWords = []
+  //   if (indices.length === 1) {
+  //     winningIndex = indices[0]
+  //     highestScore = {score: scores[winningIndex], word: words[winningIndex]}
+  //     return highestScore
+  //   } else {
+  //     for (const index of indices) {
+  //       tiedWords.push(words[index])
+  //     }
+  //     let shortest = tiedWords[0];
+  //     let shortestIndex = 0;
+  //     for (const word of tiedWords) {
+  //       if (word.length === 10) {
+  //         winningIndex = tiedWords.indexOf(word)
+  //         highestScore = {score: scores[indices[winningIndex]], word: tiedWords[winningIndex]}
+  //         return highestScore
+  //       } else {
+  //         if (word.length < shortest.length) {
+  //           shortest = word
+  //           shortestIndex = tiedWords.indexOf(shortest)
+  //         }
+  //       }
+  //     }
+  //     winningIndex = shortestIndex
     
-      highestScore = {score: scores[indices[winningIndex]], word: tiedWords[winningIndex]}
-      return highestScore
-    }
-  }
+  //     highestScore = {score: scores[indices[winningIndex]], word: tiedWords[winningIndex]}
+  //     return highestScore
+  //   }
+  // }
 };
 
 // Do not remove this line or your tests will break!
