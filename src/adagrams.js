b680cccd6a673d03104df89f85ce68070d43cc4a
Yingjing Lin <yjlin789@gmail.com>
2020-12-13 00:26:13 -0800

    fixed lowercase use scoreWord bug

--- src/adagrams.js
+++ src/adagrams.js
@@ -67,7 +67,9 @@ const Adagrams = {
     const scoreChart = {A: 1, B: 3, C: 3, D: 2, E: 1, F: 4, G: 2, H: 4, I: 1, J: 8, K: 5, L: 1, M: 3, N: 1,
                       O: 1, P: 3, Q: 10, R: 1, S: 1, T: 1, U: 1, V: 4, W: 4, X: 8, Y: 4, Z: 10};
     let score = 0;
-    word = word.toUpperCase;
+    console.log(word);
+    word = word.toUpperCase();
+    console.log(word);
     for (const char of word) {
       score += scoreChart[char];
     };
@@ -102,7 +104,6 @@ const Adagrams = {
     return winner;
   },
 };
-
 
 // Do not remove this line or your tests will break!
 export default Adagrams;
