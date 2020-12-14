fd52f059c1dcd28eba5131c3245fc0f0b314fe2e
Madeline <madelign@gmail.com>
2020-12-14 15:14:53 -0500

    scoreWord function passes all tests

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,35 +1,35 @@
 const Adagrams = {
   lettersDistribution: [
-    "A", "A", "A", "A", "A", "A", "A", "A", "A",
-    "B", "B",
-    "C", "C",
-    "D", "D", "D", "D",
-    "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E",
-    "F", "F",
-    "G", "G", "G",
-    "H", "H",
-    "I", "I", "I", "I", "I", "I", "I", "I", "I",
-    "J",
-    "K",
-    "L", "L", "L", "L",
-    "M", "M",
-    "N", "N", "N", "N", "N", "N",
-    "O", "O", "O", "O", "O", "O", "O", "O",
-    "P", "P",
-    "Q",
-    "R", "R", "R", "R", "R", "R",
-    "S", "S", "S", "S",
-    "T", "T", "T", "T", "T", "T",
-    "U", "U", "U", "U",
-    "V", "V",
-    "W", "W",
-    "X",
-    "Y", "Y",
-    "Z"
+    'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A',
+    'B', 'B',
+    'C', 'C',
+    'D', 'D', 'D', 'D',
+    'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E',
+    'F', 'F',
+    'G', 'G', 'G',
+    'H', 'H',
+    'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I',
+    'J',
+    'K',
+    'L', 'L', 'L', 'L',
+    'M', 'M',
+    'N', 'N', 'N', 'N', 'N', 'N',
+    'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+    'P', 'P',
+    'Q',
+    'R', 'R', 'R', 'R', 'R', 'R',
+    'S', 'S', 'S', 'S',
+    'T', 'T', 'T', 'T', 'T', 'T',
+    'U', 'U', 'U', 'U',
+    'V', 'V',
+    'W', 'W',
+    'X',
+    'Y', 'Y',
+    'Z'
   ],
 
-  drawLetters: function () {
-    // shuffle array
+  drawLetters() {
+    // shuffle array using Fisher-Yates Algorithm
     for (let i = this.lettersDistribution.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * i)
       const temp = this.lettersDistribution[i]
@@ -41,7 +41,7 @@ const Adagrams = {
     return this.lettersDistribution.slice(0, 10);
   },
 
-  usesAvailableLetters: function (input, lettersInHand) {
+  usesAvailableLetters(input, lettersInHand) {
     const inputLetters = input.split('');
 
     let usesAvailableLetters = true;
@@ -56,8 +56,86 @@ const Adagrams = {
     });
 
     return usesAvailableLetters;
-  }
+  },
+
+  scoreWord(word) {
+    const wordByLetter = word.toUpperCase().split('');
+
+    let score = 0;
+
+
+    if (word.length >= 7){
+      score += 8;
+    }
+
+    wordByLetter.forEach(letter => {
+      switch (letter) {
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+        case 'L':
+        case 'N':
+        case 'R':
+        case 'S':
+        case 'T': {
+          score += 1;
+          break;
+        }
+
+        case 'D':
+        case 'G': {
+          score += 2;
+          break;
+        }
+
+        case 'B':
+        case 'C':
+        case 'M':
+        case 'P': {
+          score += 3;
+          break;
+        }
+
+        case 'F':
+        case 'H':
+        case 'V':
+        case 'W':
+        case 'Y': {
+          score += 4;
+          break;
+        }
+
+        case 'K': {
+          score += 5;
+          break;
+        }
+
+        case 'J':
+        case 'X': {
+          score += 8;
+          break;
+        }
+
+        case 'Q':
+        case 'Z': {
+          score += 10;
+          break;
+        }
+
+        default:
+          console.log(`${letter} is not supported.`);
+          return;
+      }
+
+    });
+
+
+    
+    return score;
+  },
 };
 
-    // Do not remove this line or your tests will break!
-    export default Adagrams;
+// Do not remove this line or your tests will break!
+export default Adagrams;
