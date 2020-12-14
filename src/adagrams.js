33312cfe4fe3cf3aa1da8d4b9ac4041bf6b1eb76
Jessica Chan <jwinchan@gmail.com>
2020-12-14 08:06:30 -0800

    completed scoreWord method

--- src/adagrams.js
+++ src/adagrams.js
@@ -20,7 +20,7 @@ const Adagrams = {
   },
 
   usesAvailableLetters(input,lettersInHand) {
-    let lettersCopy = [...lettersInHand];
+    const lettersCopy = [...lettersInHand];
     for (const letter of input.toUpperCase()) {
       if(!lettersCopy.includes(letter)) {
         return false;
@@ -30,8 +30,59 @@ const Adagrams = {
     }
     return true;
   },
+
+  scoreWord(word) {
+    let score = 0;
+    if (word.length > 6) {
+      score += 8;
+    }
+    for (const letter of word.toUpperCase()) {
+      switch (letter) {
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+        case 'L':
+        case 'N':
+        case 'R':
+        case 'S':
+        case 'T':
+          score += 1;
+          break;
+        case 'D':
+        case 'G':
+          score += 2;
+          break;
+        case 'B':
+        case 'C':
+        case 'M':
+        case 'P':
+          score += 3;
+          break;
+        case 'F':
+        case 'H':
+        case 'V':
+        case 'W':
+        case 'Y':
+          score += 4;
+          break;
+        case 'K':
+          score += 5;
+          break;
+        case 'J':
+        case 'X':
+          score += 8;
+          break;
+        case 'Q':
+        case 'Z':
+          score += 10;
+          break;
+      }
+    }
+    return score;
+  },
 };
 
-console.log(Adagrams.drawLetters);
 // Do not remove this line or your tests will break!
 export default Adagrams;
