f80cdf24a911a33f8f2e778ff6523d1aaa0db32e
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2020-12-13 20:05:47 -0800

    created the drawLetters function for Wave 1, test is passing

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,4 @@
-const Adagrams = {
-
-  letters: {
+const letterPool = {
     A: 9,
     B: 2,
     C: 2,
@@ -27,15 +25,37 @@ const Adagrams = {
     X: 1,
     Y: 2,
     Z: 1
+};
+
+const Adagrams = {
+
+  // Wave one 
+
+  drawLetters() {
+    const letterPoolArray = [];
+
+    for(const letter in letterPool){
+      for(let i = 0; i < letterPool[letter]; i++) {
+        letterPoolArray.push(letter);
+      }
+    }
+
+    const hand = [];
+
+    for(let i = 0; i < 10; i++){
+      const randomLetters = letterPoolArray[Math.floor(Math.random() * letterPoolArray.length)];
+      hand.push(randomLetters);
+    }
+    return hand;
+  }
+
+  // Wave two 
+
+
+
+
 }
 
-// WAVE #1 
-
-  drawLetters: function() {
-    // Implement this method for wave 1
-    for
-  },
-};
 
 // Do not remove this line or your tests will break!
 export default Adagrams;
