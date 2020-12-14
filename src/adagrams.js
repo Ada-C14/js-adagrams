cdcd4643ede546dcd47b8e973eaa34b826057a7d
dnsrocha <drocha@gmail.com>
2020-12-14 10:35:32 -0800

    reversed wave 5 initial methods

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,26 +1,31 @@
-//wave 5
-class Adagrams {
+const lettersCount = {"A": 9, "B": 2, "C": 2, "D": 4, "E": 12, "F": 2, "G": 3, "H": 2, "I": 9, "J": 1, "K": 1, "L": 4, "M": 2, "N": 6, "O": 8, "P": 2, "Q": 1, "R": 6, "S": 4, "T": 6, "U": 4, "V": 2, "W": 2, "X": 1, "Y": 2, "Z": 1};
+const lettersScore = {"A": 1, "E": 1, "I": 1, "O": 1, "U": 1, "L": 1, "N": 1, "R": 1, "S": 1, "T": 1, "D": 2, "G": 2, "B": 3, "C": 3, "M": 3, "P": 3, "F": 4, "H": 4, "V": 4, "W": 4, "Y": 4,"K": 5, "J": 8, "X": 8,"Q": 10, "Z": 10};
 
-  static drawLetters() {
-    const handSize   = 10;
-    const letterCount = { "A": 9, "B": 2, "C": 2, "D": 4, "E": 12, "F": 2, "G": 3, "H": 2, "I": 9, "J": 1, "K": 1, "L":  4, "M": 2, "N": 6, "O": 8, "P": 2, "Q": 1, "R": 6, "S":  4, "T": 6, "U": 4, "V": 2, "W": 2, "X": 1, "Y": 2, "Z":  1 };
-    
-    let letterPool = "";
-    for (const [letter, value] of Object.entries(letterCount)) { 
-      letterPool += letter.repeat(value);
+const Adagrams = {
+  drawLetters() {
+    // Implement this method for wave 1
+    let total_letters = [];
+
+    for(const letter in lettersCount) {
+      for(let i = 0; i < lettersCount[letter]; i++) {
+        total_letters.push(letter);
+      };
+    };
+    return this.sampleSize(total_letters, 10);
+  },
+  
+  sampleSize(array, hand) {
+    let max = array.length;
+    while (max > 0) {
+      const i = Math.floor(Math.random() * max--);
+      [array[max], array[i]] = [array[i], array[max]];
     };
 
-    const hand = [];
-    for (let i = 0; i < handSize; i++) {
-      let randIndex  = Math.floor(Math.random() * letterPool.length);
-      let randLetter = letterPool.slice(randIndex, randIndex + 1);
-      hand.push(randLetter);
-    } 
-    return hand;
-  }
-  
+    return array.slice(0, hand);
+  },
 
   usesAvailableLetters(input, lettersInHand) {
+
     input = input.split('');
     let tally = {};
 
@@ -52,6 +57,7 @@ class Adagrams {
     for(const letter of word) {
       sum += lettersScore[letter];
     };
+
     return sum;
   },
 
@@ -70,9 +76,9 @@ class Adagrams {
 
   highestScoreFrom(words) {
     let scores = [];
+    let highScoreArray = [];
     let maxScore = 0;
     let bestWord = {};
-    let highScoreArray = [];
 
     words.forEach(word => {
       scores.push({word: word, score: this.scoreWord(word)});
@@ -100,6 +106,7 @@ class Adagrams {
   },
 
   tieBreaker(highScoreArray) {
+
     let minLength = 10;
 
     for(const word of highScoreArray) {
@@ -122,6 +129,3 @@ class Adagrams {
   },
 
 };
-
-// Do not remove this line or your tests will break!
-export default Adagrams;
